d2c9175ee5299c2dcd4a13cc5701a23adca255bb
praveenstark <praveenlightning.m@gmail.com>
2025-03-13 14:40:07 +0530

    one small change

--- lib/utils/image_cache_service.dart
+++ lib/utils/image_cache_service.dart
@@ -1,5 +1,4 @@
 import 'package:amplify_flutter/amplify_flutter.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
